790e5fd7a48e4a36fcbbb202cf30d40ed773e9d5
Anmol Gangwar <90886585+Robin-Anmol@users.noreply.github.com>
2022-10-05 18:27:17 +0530

    Update Fire.jsx

--- src/Fire.jsx
+++ src/Fire.jsx
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyAdK6zIdx5Hbxzn-BLEb2TR3d5P_qyzFJk",
-  authDomain: "netflix-clone-7fe77.firebaseapp.com",
-  projectId: "netflix-clone-7fe77",
-  storageBucket: "netflix-clone-7fe77.appspot.com",
-  messagingSenderId: "524162133702",
-  appId: "1:524162133702:web:ed73e8b3857ac96a7f31c7",
+  apiKey: "your api key",
+  authDomain: "project auth domain",
+  projectId: "project id",
+  storageBucket: "your project detail",
+  messagingSenderId: "your project detail",
+  appId: "web:your project detail",
 });
 
 const db = firebaseApp.firestore();
