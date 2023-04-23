f4a7680f4593c1177d7c86f0243f01468ea58574
Anmol Gangwar <anmolgangwar64@gmail.com>
2023-04-23 22:01:53 +0530

    env

--- src/Fire.jsx
+++ src/Fire.jsx
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "your api key",
-  authDomain: "project auth domain",
-  projectId: "project id",
-  storageBucket: "your project detail",
-  messagingSenderId: "your project detail",
-  appId: "web:your project detail",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 });
 
 const db = firebaseApp.firestore();
